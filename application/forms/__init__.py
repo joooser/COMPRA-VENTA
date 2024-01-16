3f2f49d4263847df1ac7bdb3b3454a8b7f80b9fa
Luis Alfonzo Rivas Marquez <42919821+vaslari@users.noreply.github.com>
2024-01-16 19:13:43 +0100

    fix register new user

--- application/forms/__init__.py
+++ application/forms/__init__.py
@@ -1,11 +1,9 @@
 from .LoginForm import LoginForm
 from .RegisterForm import RegisterForm
-from .QuestionsForm import VehicleSaleForm
 
 
 
 __all__ = [
     'LoginForm', 
-    'RegisterForm',
-    'VehicleSaleForm'
+    'RegisterForm'
 ]
